4a744bf9c15575a8ec24c3d77d6401459f59b8bc
ronaldo <ronaldo@cheesetea.com>
2017-03-30 22:41:48 +0200

    Added GNU GPL v3 headers

--- src/cwallNormal.h
+++ src/cwallNormal.h
@@ -1,3 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////
+//     This is part of ex_sensors, an example on how to implement sensors
+//     for enemies in C++. It is a simple textmode game that has two game modes: 
+//     Easy and Hard. This implementation adds a third mode: Sensors. In this
+//     mode, enemies created have sight sensors and stop moving when they see
+//     the player in its line of sight.
+//     This example was used during Videogame Programming lessons at the 
+//     Multimedia Degree from the University of Alicante (Spain). You may
+//     watch the associated lesson at https://youtu.be/mPapuUC0kLg
+//
+//    Copyright (C) 2017   Francisco J. Gallego-Durán
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///////////////////////////////////////////////////////////////////////////////
 #ifndef __CWALLNORMAL_H__
 #define __CWALLNORMAL_H__
 
